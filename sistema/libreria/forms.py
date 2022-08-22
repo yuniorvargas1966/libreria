6f18afab40d897d5839d7b46b9b7474d827c4be1
Yunior Vargas Hernández <yuniorvargas1966@gmail.com>
2022-08-22 14:04:19 -0400

    Add files via update

---
+++ sistema/libreria/forms.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from django import forms
+from .models import Libro
+
+class LibroForm(forms.ModelForm):
+    class Meta:
+        model = Libro
+        fields = '__all__'
